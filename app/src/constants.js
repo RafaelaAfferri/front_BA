03dd451196b872218d55e791f0e1ad50828d2750
RafaelaAfférri <rafaelaao@al.insper.edu.br>
2024-10-18 15:06:20 -0300

    rota certa

--- app/src/constants.js
+++ app/src/constants.js
@@ -1,3 +1,4 @@
 // export const rota_base = 'https://busca-ativa-emef-00fead7d18dc.herokuapp.com'
 // export const rota_base = 'http://127.0.0.1:8000'
-export const rota_base = "http://web-production-e04b.up.railway.app"
+// export const rota_base = "http://web-production-e04b.up.railway.app"
+export const rota_base = "backbarailway-production.up.railway.app"
